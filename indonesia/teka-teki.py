d4ec5e84c83631e0ca966c1081b29b01b739e524
Ray <raycunnylord@gmail.com>
2025-08-12 19:48:36 +0700

    Update teka-teki.py

--- indonesia/teka-teki.py
+++ indonesia/teka-teki.py
@@ -3,7 +3,7 @@ import os
 
 tekateki = ["Semakin kita berpaling, semakin dia menjauh… Apakah itu?", "Hewan apa yang namanya hanya ada 2 huruf?", "Apa yang berada di antara langit dan bumi?", "Bila diikat, dia akan berjalan. Bila ikatannya dibuka, maka dia akan berhenti. Siapakah itu?", "Apa yang memiliki gigi tetapi tidak bisa menggigit?", "Apa yang tidak punya jari tapi bisa menunjuk?", "Apa yang bergerak tanpa berhenti tetapi tidak pernah pergi ke mana-mana?", "Ditutup jadi tongkat, dibuka jadi tenda, apakah itu?", "Kalau sedang sendiri, kakinya ada empat, kalau berdua kakinya jadi ada delapan, siapakah aku?", "Apa yang selalu bergerak, tapi tidak pernah lelah?"]
 jawaban = ["telinga", "u dan g", "dan", "sepatu", "sisir", "kompas", "jam dinding", "payung", "kursi", "waktu"]
-acak_angka = random.randint(0, 3)
+acak_angka = random.randint(0, 9)
 
 dead = 0
 tebakan_limit = 5
