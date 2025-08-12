2bfda8435ec6709183539422eb0aa2bf3269a67d
Ray <raycunnylord@gmail.com>
2025-08-12 20:00:22 +0700

    Update teka-teki.py
    just add some quote

--- indonesia/teka-teki.py
+++ indonesia/teka-teki.py
@@ -1,13 +1,16 @@
 import random
 import os
 
+##Array pertanyaan dan jawabannya
 tekateki = ["Semakin kita berpaling, semakin dia menjauh… Apakah itu?", "Hewan apa yang namanya hanya ada 2 huruf?", "Apa yang berada di antara langit dan bumi?", "Bila diikat, dia akan berjalan. Bila ikatannya dibuka, maka dia akan berhenti. Siapakah itu?", "Apa yang memiliki gigi tetapi tidak bisa menggigit?", "Apa yang tidak punya jari tapi bisa menunjuk?", "Apa yang bergerak tanpa berhenti tetapi tidak pernah pergi ke mana-mana?", "Ditutup jadi tongkat, dibuka jadi tenda, apakah itu?", "Kalau sedang sendiri, kakinya ada empat, kalau berdua kakinya jadi ada delapan, siapakah aku?", "Apa yang selalu bergerak, tapi tidak pernah lelah?"]
 jawaban = ["telinga", "u dan g", "dan", "sepatu", "sisir", "kompas", "jam dinding", "payung", "kursi", "waktu"]
 acak_angka = random.randint(0, 9)
 
+##Variabel
 dead = 0
 tebakan_limit = 5
 
+##Fungsi loop menggunakan "while"
 while tebakan_limit >= dead:  
     print('\n"' + tekateki[acak_angka] + '"\n')
     jawab = input("Kesempatan menebak sisa: " + str(tebakan_limit) + "\nJawabanmu: ")
